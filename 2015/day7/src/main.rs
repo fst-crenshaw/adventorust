e77c23fb1c7af33e96b14fdba72aba14c7c630c1
Tanya L. Crenshaw <tcrenshaw@fastly.com>
2020-02-28 14:53:27 -0800

    Testing parsed NOT expressions.  All of AND, OR, and NOT expressions may contain variables or literals.

--- 2015/day7/src/main.rs
+++ 2015/day7/src/main.rs
@@ -2,20 +2,32 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 
-/// An Exp, or expression, takes one of the following forms:
-///   u32 -> <id>
-///   NOT <id> -> <id>
+#[derive(Debug, PartialEq)]
+enum Term<'a> {
+    Literal(u32),
+    Variable(&'a str),
+}
+
+/// An expression is any of
+///  123
+///  1 AND y
+///  x AND y
+///  0 OR x
+///  x OR 0
+///  NOT y
 #[derive(Debug, PartialEq)]
 enum Exp<'a> {
     Literal(u32),
-    UnaryExp(fn(a: u32) -> u32, &'a str),
-    BinaryExp(fn(a: u32, b: u32) -> u32, &'a str, &'a str),
+    UnaryExp(fn(a: u32) -> u32, Term<'a>),
+    BinaryExp(fn(a: u32, b: u32) -> u32, Term<'a>, Term<'a>),
 }
 
-/// An Assignment is a variable and an expression.
+/// An Assignment is an identifier and an expression.
+///    i.e., <exp> -> <id>
+///
 #[derive(Debug, PartialEq)]
 struct Assignment<'a> {
-    var: &'a str,
+    id: &'a str,
     exp: Exp<'a>,
 }
 
@@ -36,6 +48,21 @@ fn aoc_not(a: u32) -> u32 {
     return !a;
 }
 
+/// Given a string representing a term in an expression
+/// return its type, either a Variable (like "x") or
+/// a Literal (like 1).
+fn reduce<'a>(s: &'a str) -> Term<'a> {
+    let term = s.clone();
+
+    // Is it a number?
+    let maybe_number = term.parse::<u32>();
+
+    match maybe_number {
+        Ok(number) => return Term::Literal(number),
+        Err(_) => return Term::Variable(term),
+    }
+}
+
 fn parse<'a>(s: &'a str) -> Result<Box<Assignment<'a>>, ()> {
     let exp;
     let cap;
@@ -43,38 +70,38 @@ fn parse<'a>(s: &'a str) -> Result<Box<Assignment<'a>>, ()> {
     // Parse the assignment `<id> AND <id> -> <id>`
     if s.contains("AND") {
         let re =
-            Regex::new(r"^(?P<exp1>\w{1,2}) AND (?P<exp2>\w{1,2}) -> (?P<var>\w{1,2})$").unwrap();
+            Regex::new(r"^(?P<exp1>\w{1,2}) AND (?P<exp2>\w{1,2}) -> (?P<id>\w{1,2})$").unwrap();
         cap = re.captures(&s).unwrap();
         exp = Exp::BinaryExp(
             aoc_and,
-            cap.name("exp1").unwrap().as_str(),
-            cap.name("exp2").unwrap().as_str(),
+            reduce(cap.name("exp1").unwrap().as_str()),
+            reduce(cap.name("exp2").unwrap().as_str()),
         );
     }
     // Parse the assignment `<id> OR <id> -> <id>`
     else if s.contains("OR") {
         let re =
-            Regex::new(r"^(?P<exp1>\w{1,2}) OR (?P<exp2>\w{1,2}) -> (?P<var>\w{1,2})$").unwrap();
+            Regex::new(r"^(?P<exp1>\w{1,2}) OR (?P<exp2>\w{1,2}) -> (?P<id>\w{1,2})$").unwrap();
         cap = re.captures(&s).unwrap();
         exp = Exp::BinaryExp(
             aoc_or,
-            cap.name("exp1").unwrap().as_str(),
-            cap.name("exp2").unwrap().as_str(),
+            reduce(cap.name("exp1").unwrap().as_str()),
+            reduce(cap.name("exp2").unwrap().as_str()),
         );
     }
     // Parse the assignment `NOT <id> -> <id>`
     else if s.contains("NOT") {
-        let re = Regex::new(r"^NOT (?P<exp>\w{1,2}) -> (?P<var>\w{1,2})$").unwrap();
+        let re = Regex::new(r"^NOT (?P<exp>\w{1,2}) -> (?P<id>\w{1,2})$").unwrap();
         cap = re.captures(&s).unwrap();
-        exp = Exp::UnaryExp(aoc_not, cap.name("exp").unwrap().as_str());
+        exp = Exp::UnaryExp(aoc_not, reduce(cap.name("exp").unwrap().as_str()));
     // Parse the assignment `u32 -> <id>`
     } else {
-        let re = Regex::new(r"^(?P<literal>\d{1,4}) -> (?P<var>\w{1,2})$").unwrap();
+        let re = Regex::new(r"^(?P<literal>\d{1,4}) -> (?P<id>\w{1,2})$").unwrap();
         cap = re.captures(&s).unwrap();
         exp = Exp::Literal(cap.name("literal").unwrap().as_str().parse().unwrap());
     }
     let assign = Assignment {
-        var: &cap.name("var").unwrap().as_str(),
+        id: &cap.name("id").unwrap().as_str(),
         exp,
     };
     return Ok(Box::new(assign));
@@ -105,20 +132,20 @@ fn main() {
     for line in s.split('\n') {
         let assignment = parse(line).unwrap();
         println!("{:?}", assignment);
-        expressions.insert(assignment.var, assignment.exp);
+        expressions.insert(assignment.id, assignment.exp);
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{aoc_and, aoc_or, parse, Assignment, Exp};
+    use crate::{aoc_and, aoc_not, aoc_or, parse, Assignment, Exp, Term};
 
     #[test]
     fn parse_simple_assignment() {
         assert_eq!(
             parse("123 -> x").unwrap(),
             Box::new(Assignment {
-                var: "x",
+                id: "x",
                 exp: Exp::Literal(123)
             })
         );
@@ -129,8 +156,19 @@ mod tests {
         assert_eq!(
             parse("x AND y -> d").unwrap(),
             Box::new(Assignment {
-                var: "d",
-                exp: Exp::BinaryExp(aoc_and, "x", "y")
+                id: "d",
+                exp: Exp::BinaryExp(aoc_and, Term::Variable("x"), Term::Variable("y"))
+            })
+        );
+    }
+
+    #[test]
+    fn parse_assignment_with_and_with_literal() {
+        assert_eq!(
+            parse("1 AND y -> d").unwrap(),
+            Box::new(Assignment {
+                id: "d",
+                exp: Exp::BinaryExp(aoc_and, Term::Literal(1), Term::Variable("y"))
             })
         );
     }
@@ -140,8 +178,37 @@ mod tests {
         assert_eq!(
             parse("x OR y -> e").unwrap(),
             Box::new(Assignment {
-                var: "e",
-                exp: Exp::BinaryExp(aoc_or, "x", "y")
+                id: "e",
+                exp: Exp::BinaryExp(aoc_or, Term::Variable("x"), Term::Variable("y"))
+            })
+        );
+    }
+
+    #[test]
+    fn parse_assignment_with_or_with_literal() {
+        assert_eq!(
+            parse("0 OR y -> e").unwrap(),
+            Box::new(Assignment {
+                id: "e",
+                exp: Exp::BinaryExp(aoc_or, Term::Literal(0), Term::Variable("y"))
+            })
+        );
+        assert_eq!(
+            parse("y OR 0 -> e").unwrap(),
+            Box::new(Assignment {
+                id: "e",
+                exp: Exp::BinaryExp(aoc_or, Term::Variable("y"), Term::Literal(0))
+            })
+        );
+    }
+
+    #[test]
+    fn parse_assignment_with_not() {
+        assert_eq!(
+            parse("NOT y -> e").unwrap(),
+            Box::new(Assignment {
+                id: "e",
+                exp: Exp::UnaryExp(aoc_not, Term::Variable("y"))
             })
         );
     }
