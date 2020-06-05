f09ba1a79dfb65ffeba07316228ff04bf4ebb0d5
Tanya L. Crenshaw <tcrenshaw@fastly.com>
2020-06-05 16:24:46 -0700

    Comment fully the main.rs demonstrating the trait challenge.

--- misc/traits/src/main.rs
+++ misc/traits/src/main.rs
@@ -2,6 +2,17 @@ use clap::{App, Arg};
 use num_traits::pow;
 use std;
 
+/// This command line application accepts the name of a shape --
+/// either circle or square -- and then calculates the area of
+/// that shape based on the supplied n.
+
+/// Example:
+///   $ cargo run -- --params diameter=12 name=square
+///       144
+///
+///   $ cargo run -- --params diameter=12 name=circle
+///       113.097
+
 // Generic way to describe the necessary behavior of a shape.
 trait Shape {
     fn new(n: u32) -> Self;
@@ -38,7 +49,6 @@ impl Shape for Square {
     }    
 }
 
-
 #[derive(Debug, Default)]
 struct Params {
     name: String,
@@ -67,6 +77,8 @@ impl std::str::FromStr for KvPair {
 }
 
 fn main() {
+
+    // Collect the command line parameters using clap.
     let matches = App::new("shapes")
         .arg(
             Arg::with_name("params")
@@ -77,9 +89,7 @@ fn main() {
         )
         .get_matches();
 
-    // dbg!(matches);
-
-    
+    // Create a vector of KvPairs
     let stuff: Vec<KvPair> = matches
         .values_of("params")
         .unwrap()
@@ -87,8 +97,7 @@ fn main() {
 	.map(|s| s.parse().unwrap())
 	.collect();
 
-    // dbg!(stuff);
-    
+    // Convert the KvPairs to a Params struct. 
     let params = {
         let mut params = Params::default();
         for KvPair { key, value } in stuff {
