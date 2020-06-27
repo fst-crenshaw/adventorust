ad42296979f62169c4488f3e90f8c6b238aad296
Tanya L. Crenshaw <tcrenshaw@fastly.com>
2020-06-26 17:49:56 -0700

    Compiling with snappy.

--- misc/ffi/build.rs
+++ misc/ffi/build.rs
@@ -2,19 +2,19 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let project_root = {
-        let mut buf = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        buf.pop();
-        buf
-    };
-
+    let project_root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+   
     let snappy_dir = project_root.join("vendor").join("snappy");
 
-    println!(
-        "cargo:rustc-link-search={}",
-        snappy_dir.join("build").display()
-    );    
-    println!("cargo:rustc-link-lib=static=snappy");
+    // The following is equilvalent to the "-L" flag in rustc.  It
+    // indicates the directory in which cargo ought to search for
+    // libraries to link with ~/src/.  In this case, Link ~/src/ with
+    // libraries located in ~/vendor/snappy/build.
+    println!("cargo:rustc-link-search={}", snappy_dir.join("build").display());    
+    // The following is equivalent to the "-l" flag in rustc.  It
+    // indicates the library with which cargo ought to link
+    // with ~/src/.
+    println!("cargo:rustc-link-lib=snappy");
 
- }
+}
 
