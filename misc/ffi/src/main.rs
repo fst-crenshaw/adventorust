5686f4591782c2289c786a3f1c75fc2f2270fad0
Tanya L. Crenshaw <tcrenshaw@fastly.com>
2020-06-26 18:01:40 -0700

    .

--- misc/ffi/src/main.rs
+++ misc/ffi/src/main.rs
@@ -2,18 +2,7 @@ extern crate libc;
 use libc::{c_int, size_t};
 
 extern {
-    fn snappy_compress(input: *const u8,
-                       input_length: size_t,
-                       compressed: *mut u8,
-                       compressed_length: *mut size_t) -> c_int;
-    fn snappy_uncompress(compressed: *const u8,
-                         compressed_length: size_t,
-                         uncompressed: *mut u8,
-                         uncompressed_length: *mut size_t) -> c_int;
     fn snappy_max_compressed_length(source_length: size_t) -> size_t;
-    fn snappy_uncompressed_length(compressed: *const u8,
-                                  compressed_length: size_t,
-                                  result: *mut size_t) -> c_int;
     fn snappy_validate_compressed_buffer(compressed: *const u8,
                                          compressed_length: size_t) -> c_int;
 }
