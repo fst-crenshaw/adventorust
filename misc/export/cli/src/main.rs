d2cd789bab667ad3295d2d2b981eed08a4937db5
Tanya L. Crenshaw <tcrenshaw@fastly.com>
2020-11-06 14:06:52 -0800

    Working example for exporting a constant.

--- misc/export/cli/src/main.rs
+++ misc/export/cli/src/main.rs
@@ -1,5 +1,6 @@
-use lib::{public_function};
+use lib::{public_function, SPECIAL_CONSTANT};
 
+/*
 #[path = "../../share.rs"]
 // Question: Why is this a mod here?  Why not use?
 // Question: If we'd like to share SPECIAL CONSTANT to the outside world, how do we do that?
@@ -7,9 +8,9 @@ use lib::{public_function};
 // The crate in pub(crate) is like scoping the constant to the crate.  One can also supply
 // `super` and `self` to provide different levels of scope.
 pub(crate) mod share;
-
+*/
 
 fn main() {
-    println!("Hello, world {:?}!", share::SPECIAL_CONSTANT);
+    println!("Hello, world {:?}!", SPECIAL_CONSTANT);
     public_function();
 }
--- misc/export/lib/src/lib.rs
+++ misc/export/lib/src/lib.rs
@@ -1,7 +1,14 @@
 #[path = "../../share.rs"]
-pub(in super) mod share;
+pub(crate) mod share;      // including the constant.  Exporting the constant?
 //pub(crate) mod share;
+
+// Include the special constant from the share.
+//use share::SPECIAL_CONSTANT;
 
 pub fn public_function() {
     println!("{:?}: `public_function()`", share::SPECIAL_CONSTANT);
+    println!("I am using it now: {:?}.", SPECIAL_CONSTANT);
 }
+
+// Export the constant for our friends to use.
+pub use share::SPECIAL_CONSTANT;
