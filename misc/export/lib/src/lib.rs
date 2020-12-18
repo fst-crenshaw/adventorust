c7ae61e17cf5d3cd2e037af9715c2a0d16ed3e28
Tanya L. Crenshaw <tcrenshaw@fastly.com>
2020-12-18 12:33:01 -0800

    Add changes to lib.rs.

--- misc/export/lib/src/lib.rs
+++ misc/export/lib/src/lib.rs
@@ -1,14 +1,14 @@
 #[path = "../../share.rs"]
-pub(crate) mod share;      // including the constant.  Exporting the constant?
+pub(crate) mod foo;      // including the constant.  Exporting the constant?
 //pub(crate) mod share;
 
 // Include the special constant from the share.
 //use share::SPECIAL_CONSTANT;
 
 pub fn public_function() {
-    println!("{:?}: `public_function()`", share::SPECIAL_CONSTANT);
+    //println!("{:?}: `public_function()`", share::SPECIAL_CONSTANT);
     println!("I am using it now: {:?}.", SPECIAL_CONSTANT);
 }
 
 // Export the constant for our friends to use.
-pub use share::SPECIAL_CONSTANT;
+pub use foo::SPECIAL_CONSTANT;
